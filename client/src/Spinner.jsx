f135a489b1eab314dc6c776fa40a10453c92177e
Watto <watto@localhost.localdomain>
2019-09-12 23:38:42 +0530

    Second

--- client/src/Spinner.jsx
+++ client/src/Spinner.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './App.css';
 function Spinner() {
 	return (
-<div class="lds-dual-ring"></div>	);
-}
+
+<div class="sk-cube-grid">
+  <div class="sk-cube sk-cube1"></div>
+  <div class="sk-cube sk-cube2"></div>
+  <div class="sk-cube sk-cube3"></div>
+  <div class="sk-cube sk-cube4"></div>
+  <div class="sk-cube sk-cube5"></div>
+  <div class="sk-cube sk-cube6"></div>
+  <div class="sk-cube sk-cube7"></div>
+  <div class="sk-cube sk-cube8"></div>
+  <div class="sk-cube sk-cube9"></div>
+</div>
+
+);}
 
 export default Spinner;
